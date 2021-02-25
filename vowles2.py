d28f58fcfcb96bce58d8e357c15be4b80177680e
cge1111 <78770239+cge1111@users.noreply.github.com>
2021-02-25 17:37:22 +0900

    Add files via upload

---
+++ vowles2.py
@@ -0,0 +1,13 @@
+vowels = ['a','e','i','o','u']
+
+word = 'goeun'
+found=[]
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+
+for vowel in found:
+    print(vowel)
+
